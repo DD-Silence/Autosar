46dc34f72aa58392489b2f32268dc9dc09f068aa
DD-Silence <ddsilence@sina.cn>
2022-04-25 23:36:49 +0800

    Initial commit

---
+++ Abstract.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autosar
+{
+    public interface IArIdentifier
+    {
+        IDENTIFIER SHORTNAME { get; set; }
+    }
+
+    public interface IArEcucBswmdBase : IArIdentifier
+    {
+        MULTILANGUAGEOVERVIEWPARAGRAPH DESC { get; set; }
+        POSITIVEINTEGERVALUEVARIATIONPOINT LOWERMULTIPLICITY { get; set; }
+        POSITIVEINTEGERVALUEVARIATIONPOINT UPPERMULTIPLICITY { get; set; }
+        BOOLEANVALUEVARIATIONPOINT UPPERMULTIPLICITYINFINITE { get; set; }
+        DOCUMENTATIONBLOCK INTRODUCTION { get; set; }
+    }
+
+    public partial class ARPACKAGE : IArIdentifier
+    {
+    }
+
+    public partial class ECUCMODULEDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCPARAMCONFCONTAINERDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCCHOICECONTAINERDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCENUMERATIONPARAMDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCINTEGERPARAMDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCBOOLEANPARAMDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCFLOATPARAMDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCSTRINGPARAMDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCFUNCTIONNAMEDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCREFERENCEDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCFOREIGNREFERENCEDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCSYMBOLICNAMEREFERENCEDEF : IArEcucBswmdBase
+    {
+    }
+
+    public partial class ECUCCHOICEREFERENCEDEF : IArEcucBswmdBase
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Autosar Serializer and Deserializer
+    Autosar Serializer and Deserializer is base on LinqToXsdCore.
+    Autosar is complaint with Autosar V4.4 and AUTOSAR_00046.xsd.
+
+    The code is generated through the following procedure,
+    1. LinqToXsdCore is published as a global .NET tool here:?https://www.nuget.org/packages/LinqToXsdCore.
+    2. Install LinqToXsdCore 
+    dotnet tool install LinqToXsdCore -g
+    3. Create config file 
+    linqtoxsd config -e AUTOSAR_00046.xsd
+    4. Generate code 
+    linqtoxsd gen AUTOSAR_00046.xsd -c .\AUTOSAR_00046.xsd.config
+
+Feel free to try the function and feedback by issues.
